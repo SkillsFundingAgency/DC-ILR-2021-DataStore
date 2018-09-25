dc8df85d9d5173566f8dad8b154478a7b6d09c67
jamie <jamie.coales@education.gov.uk>
2018-09-25 11:42:57 +0100

    #59968 implement fm36 datastore

--- src/ESFA.DC.ILR1819.DataStore.PersistData/Services/Providers/FM36ProviderService.cs
+++ src/ESFA.DC.ILR1819.DataStore.PersistData/Services/Providers/FM36ProviderService.cs
@@ -1,5 +1,5 @@
 ﻿using Autofac.Features.AttributeFilters;
-using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model;
+using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
 using ESFA.DC.ILR1819.DataStore.Dto;
 using ESFA.DC.ILR1819.DataStore.Interface.Service;
 using ESFA.DC.IO.Interfaces;
@@ -9,7 +9,7 @@ using ESFA.DC.Serialization.Interfaces;
 
 namespace ESFA.DC.ILR1819.DataStore.PersistData.Services.Providers
 {
-    public class FM36ProviderService : BaseFundingModelProviderService<FM36FundingOutputs>, IFM36ProviderService
+    public class FM36ProviderService : BaseFundingModelProviderService<FM36Global>, IFM36ProviderService
     {
         public FM36ProviderService(
             [KeyFilter(PersistenceStorageKeys.Redis)]
@@ -21,7 +21,7 @@ namespace ESFA.DC.ILR1819.DataStore.PersistData.Services.Providers
             _jsonSerializationService = jsonSerializationService;
             _logger = logger;
 
-            _key = JobContextMessageKey.FundingFm35Output;
+            _key = JobContextMessageKey.FundingFm36Output;
             _name = "FM36";
         }
     }
