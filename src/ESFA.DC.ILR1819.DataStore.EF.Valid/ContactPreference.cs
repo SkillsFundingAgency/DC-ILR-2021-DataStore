17b19ec3bbdf6c6feb5567443c9060c0f94aed63
kelvin.morris <kelvin.morris@eas.co.uk>
2019-03-12 15:53:47 +0000

    #71171 Added missing Valid context

---
+++ src/ESFA.DC.ILR1819.DataStore.EF.Valid/ContactPreference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ESFA.DC.ILR1819.DataStore.EF.Valid
+{
+    public partial class ContactPreference
+    {
+        public int UKPRN { get; set; }
+        public string LearnRefNumber { get; set; }
+        public string ContPrefType { get; set; }
+        public int ContPrefCode { get; set; }
+
+        public virtual Learner Learner { get; set; }
+    }
+}
