81bb113baabd65637d77daffd060ddc02c22f6a6
Mike Childs <mike@mjcdev.co.uk>
2019-05-10 11:14:42 +0100

    Plumbing Database Deployment Service and Unit Tests

--- src/ESFA.DC.ILR.DataStore.Desktop/BuildDataStoreDesktopTask.cs
+++ src/ESFA.DC.ILR.DataStore.Desktop/BuildDataStoreDesktopTask.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using ESFA.DC.ILR.DataStore.Desktop.Service.Interface;
+using ESFA.DC.ILR.DataStore.Interface;
 using ESFA.DC.ILR.Desktop.Interface;
 
 namespace ESFA.DC.ILR.DataStore.Desktop
 {
     public class BuildDataStoreDesktopTask : IDesktopTask
     {
-        public Task<IDesktopContext> ExecuteAsync(IDesktopContext desktopContext, CancellationToken cancellationToken)
+        private readonly IDatabaseDeploymentService _databaseDeploymentService;
+        private readonly IDataStoreContextFactory<IDesktopContext> _dataStoreContextFactory;
+
+        public BuildDataStoreDesktopTask(IDatabaseDeploymentService databaseDeploymentService, IDataStoreContextFactory<IDesktopContext> dataStoreContextFactory)
         {
-            throw new NotImplementedException();
+            _databaseDeploymentService = databaseDeploymentService;
+            _dataStoreContextFactory = dataStoreContextFactory;
+        }
+
+        public async Task<IDesktopContext> ExecuteAsync(IDesktopContext desktopContext, CancellationToken cancellationToken)
+        {
+            var dataStoreContext = _dataStoreContextFactory.Build(desktopContext);
+
+            await _databaseDeploymentService.DeployAsync(dataStoreContext.IlrDatabaseConnectionString, cancellationToken);
+
+            return desktopContext;
         }
     }
 }
---
+++ src/ESFA.DC.ILR.DataStore.Desktop/Service/ILRDatabaseDeploymentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.DataStore.Desktop.Service.Interface;
+
+namespace ESFA.DC.ILR.DataStore.Desktop.Service
+{
+    public class ILRDatabaseDeploymentService : IDatabaseDeploymentService
+    {
+        public Task DeployAsync(string connectionString, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
