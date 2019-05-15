ca4df95127c785d666443255ca21defdaa2b1c44
Mike Childs <mike@mjcdev.co.uk>
2019-05-15 11:07:51 +0100

    Create Datastore Desktop Module and Tidy Up Nuget Config, un-64 bit the dependent Libraries where possible

---
+++ src/ESFA.DC.ILR.DataStore.Desktop/Modules/BuildDataStoreModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using ESFA.DC.ILR.DataStore.Desktop.Context;
+using ESFA.DC.ILR.DataStore.Desktop.Service;
+using ESFA.DC.ILR.DataStore.Desktop.Service.Interface;
+using ESFA.DC.ILR.DataStore.Interface;
+using ESFA.DC.ILR.Desktop.Interface;
+
+namespace ESFA.DC.ILR.DataStore.Desktop.Modules
+{
+    public class BuildDataStoreModule : Module
+    {
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<ILRDatabaseDeploymentService>().As<IDatabaseDeploymentService>();
+
+            containerBuilder.RegisterType<DataStoreContextFactory>().As<IDataStoreContextFactory<IDesktopContext>>();
+        }
+    }
+}
