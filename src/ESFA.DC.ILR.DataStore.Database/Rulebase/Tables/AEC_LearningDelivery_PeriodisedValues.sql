e4c9118dfe2316cdd8598e5226664b36e3bfd996
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-06-14 13:23:30 +0100

    76511 - AEC(FM36) output added

--- src/ESFA.DC.ILR.DataStore.Database/Rulebase/Tables/AEC_LearningDelivery_PeriodisedValues.sql
+++ src/ESFA.DC.ILR.DataStore.Database/Rulebase/Tables/AEC_LearningDelivery_PeriodisedValues.sql
@@ -1,27 +1,27 @@
-﻿--CREATE TABLE [Rulebase].[AEC_LearningDelivery_PeriodisedValues] (
---    [UKPRN]          INT             NOT NULL,
---    [LearnRefNumber] VARCHAR (12)    NOT NULL,
---    [AimSeqNumber]   INT             NOT NULL,
---    [AttributeName]  VARCHAR (100)   NOT NULL,
---    [Period_1]       DECIMAL (15, 5) NULL,
---    [Period_2]       DECIMAL (15, 5) NULL,
---    [Period_3]       DECIMAL (15, 5) NULL,
---    [Period_4]       DECIMAL (15, 5) NULL,
---    [Period_5]       DECIMAL (15, 5) NULL,
---    [Period_6]       DECIMAL (15, 5) NULL,
---    [Period_7]       DECIMAL (15, 5) NULL,
---    [Period_8]       DECIMAL (15, 5) NULL,
---    [Period_9]       DECIMAL (15, 5) NULL,
---    [Period_10]      DECIMAL (15, 5) NULL,
---    [Period_11]      DECIMAL (15, 5) NULL,
---    [Period_12]      DECIMAL (15, 5) NULL,
---    PRIMARY KEY CLUSTERED ([UKPRN] ASC, [LearnRefNumber] ASC, [AimSeqNumber] ASC, [AttributeName] ASC)
---);
---GO
+﻿CREATE TABLE [Rulebase].[AEC_LearningDelivery_PeriodisedValues] (
+    [UKPRN]          INT             NOT NULL,
+    [LearnRefNumber] VARCHAR (12)    NOT NULL,
+    [AimSeqNumber]   INT             NOT NULL,
+    [AttributeName]  VARCHAR (100)   NOT NULL,
+    [Period_1]       DECIMAL (12, 5) NULL,
+    [Period_2]       DECIMAL (12, 5) NULL,
+    [Period_3]       DECIMAL (12, 5) NULL,
+    [Period_4]       DECIMAL (12, 5) NULL,
+    [Period_5]       DECIMAL (12, 5) NULL,
+    [Period_6]       DECIMAL (12, 5) NULL,
+    [Period_7]       DECIMAL (12, 5) NULL,
+    [Period_8]       DECIMAL (12, 5) NULL,
+    [Period_9]       DECIMAL (12, 5) NULL,
+    [Period_10]      DECIMAL (12, 5) NULL,
+    [Period_11]      DECIMAL (12, 5) NULL,
+    [Period_12]      DECIMAL (12, 5) NULL,
+    PRIMARY KEY CLUSTERED ([UKPRN] ASC, [LearnRefNumber] ASC, [AimSeqNumber] ASC, [AttributeName] ASC)
+);
+GO
 
---ALTER TABLE [Rulebase].[AEC_LearningDelivery_PeriodisedValues] ADD CONSTRAINT [FK_AECLearningDeliveryPeriodisedValues_AECLearningDeliveryPeriod] FOREIGN KEY([UKPRN], [LearnRefNumber], [AimSeqNumber])
---REFERENCES [Rulebase].[AEC_LearningDelivery] ([UKPRN], [LearnRefNumber], [AimSeqNumber]);
---GO
+ALTER TABLE [Rulebase].[AEC_LearningDelivery_PeriodisedValues] ADD CONSTRAINT [FK_AECLearningDeliveryPeriodisedValues_AECLearningDeliveryPeriod] FOREIGN KEY([UKPRN], [LearnRefNumber], [AimSeqNumber])
+REFERENCES [Rulebase].[AEC_LearningDelivery] ([UKPRN], [LearnRefNumber], [AimSeqNumber]);
+GO
 
---ALTER TABLE [Rulebase].[AEC_LearningDelivery_PeriodisedValues] CHECK CONSTRAINT [FK_AECLearningDeliveryPeriodisedValues_AECLearningDeliveryPeriod]
---GO
+ALTER TABLE [Rulebase].[AEC_LearningDelivery_PeriodisedValues] CHECK CONSTRAINT [FK_AECLearningDeliveryPeriodisedValues_AECLearningDeliveryPeriod]
+GO
