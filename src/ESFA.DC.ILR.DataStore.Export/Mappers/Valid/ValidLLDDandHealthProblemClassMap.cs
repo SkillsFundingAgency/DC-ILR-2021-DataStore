806f6acdb4bef6ea53c398e61d0c04d31764c08a
kelvin.morris <kelvin.morris@eas.co.uk>
2019-11-01 10:01:00 +0000

    #89262 removed comments from mapping class

--- src/ESFA.DC.ILR.DataStore.Export/Mappers/Valid/ValidLLDDandHealthProblemClassMap.cs
+++ src/ESFA.DC.ILR.DataStore.Export/Mappers/Valid/ValidLLDDandHealthProblemClassMap.cs
@@ -8,12 +8,6 @@ namespace ESFA.DC.ILR.DataStore.Export.Mappers.Valid
         public ValidLLDDandHealthProblemClassMap()
         {
             Map(m => m.Learner).Ignore();
-
-            //Map(m => m.UKPRN);
-            //Map(m => m.LearnRefNumber);
-            //Map(m => m.LLDDCat);
-            //Map(m => m.LLDDandHealthProblem_ID);
-            //Map(m => m.PrimaryLLDD);
         }
     }
 }
