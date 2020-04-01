0cd00d948f14f6d11af5f07e9727e455a233a9a5
Ashley Beecham <ash@momentoussolutions.co.uk>
2020-04-01 12:53:20 +0100

    Initial SF config rollover changes

--- src/ESFA.DC.ILR.DataStore.Stateless/Modules/IOModule.cs
+++ src/ESFA.DC.ILR.DataStore.Stateless/Modules/IOModule.cs
@@ -1,36 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autofac;
+﻿using Autofac;
 using ESFA.DC.FileService;
 using ESFA.DC.FileService.Config.Interface;
 using ESFA.DC.FileService.Interface;
-using ESFA.DC.IO.AzureStorage;
-using ESFA.DC.IO.AzureStorage.Config.Interfaces;
-using ESFA.DC.IO.Interfaces;
 
 namespace ESFA.DC.ILR.DataStore.Stateless.Modules
 {
     public class IOModule : Module
     {
         private readonly IAzureStorageFileServiceConfiguration _azureStorageFileServiceConfig;
-        private readonly IAzureStorageKeyValuePersistenceServiceConfig _azureStorageKeyValuePersistenceServiceConfig;
 
-        public IOModule(IAzureStorageFileServiceConfiguration azureStorageFileServiceConfig, IAzureStorageKeyValuePersistenceServiceConfig azureStorageKeyValuePersistenceServiceConfig)
+        public IOModule(IAzureStorageFileServiceConfiguration azureStorageFileServiceConfig)
         {
             _azureStorageFileServiceConfig = azureStorageFileServiceConfig;
-            _azureStorageKeyValuePersistenceServiceConfig = azureStorageKeyValuePersistenceServiceConfig;
         }
 
         protected override void Load(ContainerBuilder containerBuilder)
         {
             containerBuilder.RegisterInstance(_azureStorageFileServiceConfig).As<IAzureStorageFileServiceConfiguration>();
-            containerBuilder.RegisterInstance(_azureStorageKeyValuePersistenceServiceConfig).As<IAzureStorageKeyValuePersistenceServiceConfig>();
 
             containerBuilder.RegisterType<AzureStorageFileService>().As<IFileService>();
-            containerBuilder.RegisterType<AzureStorageKeyValuePersistenceService>().As<IKeyValuePersistenceService>();
         }
     }
 }
