c2b5a16b2404bd6caab5e3b25acb4927fa5fa78a
Ashley Beecham <ash@momentoussolutions.co.uk>
2020-06-15 11:35:39 +0100

    IgnoreReference added to csv export as inline argument. did not appear to work when behing handled in a config class.

--- src/ESFA.DC.ILR.DataStore.Export/CsvExport.cs
+++ src/ESFA.DC.ILR.DataStore.Export/CsvExport.cs
@@ -23,33 +23,17 @@ namespace ESFA.DC.ILR.DataStore.Export
 
         public async Task ExportAsync<T, TClassMap>(string tableName, IEnumerable<T> source, string exportPath, OleDbConnection connection, OleDbTransaction transaction, CancellationToken cancellationToken) where TClassMap : ClassMap
         {
-            var configuration = GetConfiguration<TClassMap>();
-
             using (var writer = new StreamWriter(await _fileService.OpenWriteStreamAsync($"{tableName}.csv", exportPath, cancellationToken)))
-            using (var csv = new CsvWriter(writer, configuration))
+            using (var csv = new CsvWriter(writer))
             {
-                csv.Configuration.RegisterClassMap<TClassMap>();
+                csv.Configuration.IgnoreReferences = true;
                 csv.Configuration.ShouldQuote = (field, context) => true;
-
                 csv.WriteRecords(source);
 
                 csv.Configuration.UnregisterClassMap<TClassMap>();
             }
 
             await _decoratedExport.ExportAsync<T, TClassMap>(tableName, source, exportPath, connection, transaction, cancellationToken);
-        }
-
-        private Configuration GetConfiguration<TClassMap>()
-            where TClassMap : ClassMap
-        {
-            var configuration = new Configuration()
-            {
-                IgnoreReferences = true,
-                ShouldQuote = (field, context) => true
-            };
-
-            configuration.RegisterClassMap<TClassMap>();
-            return configuration;
         }
     }
 }
